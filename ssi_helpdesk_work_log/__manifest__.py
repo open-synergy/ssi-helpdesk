e25a1eee65285dd24c4fb9d6e032f3206cd4acf5
Andhitia Rama <andhitia.r@gmail.com>
2022-12-16 22:25:21 +0700

    [ssi_helpdesk_work_log] 14.0.1.0.0

---
+++ ssi_helpdesk_work_log/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+{
+    "name": "Helpdesk - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_helpdesk",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
