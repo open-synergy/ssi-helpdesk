de759729cb45aa2ab7118994ab4e0801a541834d
Andhitia Rama <andhitia.r@gmail.com>
2022-12-17 04:28:29 +0700

    [ssi_helpdesk_project] 14.0.1.0.0

---
+++ ssi_helpdesk_project/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+{
+    "name": "Helpdesk - Project Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_helpdesk",
+        "ssi_project",
+    ],
+    "data": [
+        "views/project_task_views.xml",
+        "views/helpdesk_ticket_views.xml",
+    ],
+    "demo": [],
+    "images": [],
+}
