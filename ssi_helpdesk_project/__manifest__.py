7c0b49e24ef93327a6480c4d6a3714659c910b1f
Andhitia Rama <andhitia.r@gmail.com>
2023-01-02 04:34:22 +0700

    [ssi_helpdesk_project] 14.0.2.0.0

--- ssi_helpdesk_project/__manifest__.py
+++ ssi_helpdesk_project/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
 {
     "name": "Helpdesk - Project Integration",
-    "version": "14.0.1.1.0",
+    "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -11,6 +11,7 @@
     "depends": [
         "ssi_helpdesk",
         "ssi_project",
+        "ssi_task_timebox",
     ],
     "data": [
         "views/project_task_views.xml",
--- ssi_helpdesk_project/models/helpdesk_ticket.py
+++ ssi_helpdesk_project/models/helpdesk_ticket.py
@@ -22,6 +22,43 @@ class HelpdeskTicket(models.Model):
         column1="ticket_id",
         column2="task_id",
     )
+    total_task = fields.Integer(
+        string="Total Task",
+        compute="_compute_task",
+        store=True,
+    )
+    timebox_latest_id = fields.Many2one(
+        string="Letest Timebox",
+        comodel_name="project.timebox",
+        compute="_compute_timebox",
+        store=True,
+    )
+    timebox_latest_date_start = fields.Date(
+        string="Latest Timebox Date Start",
+        compute="_compute_timebox",
+        store=True,
+    )
+    timebox_latest_date_end = fields.Date(
+        string="Lates Timebox Date End",
+        compute="_compute_timebox",
+        store=True,
+    )
+    timebox_initial_id = fields.Many2one(
+        string="Timebox Initial",
+        comodel_name="project.timebox",
+        compute="_compute_timebox",
+        store=True,
+    )
+    timebox_initial_date_start = fields.Date(
+        string="Timebox Initial Date Start",
+        compute="_compute_timebox",
+        store=True,
+    )
+    timebox_initial_date_end = fields.Date(
+        string="Timebox Initial Date End",
+        compute="_compute_timebox",
+        store=True,
+    )
 
     @api.depends(
         "task_ids",
@@ -31,8 +68,42 @@ class HelpdeskTicket(models.Model):
             total_task = len(record.task_ids)
             record.total_task = total_task
 
-    total_task = fields.Integer(
-        string="Total Task",
-        compute="_compute_task",
-        store=True,
+    @api.depends(
+        "task_ids",
+        "task_ids.timebox_latest_id",
+        "task_ids.timebox_date_start",
+        "task_ids.timebox_date_end",
+        "task_ids.timebox_initial_id",
+        "task_ids.timebox_initial_date_start",
+        "task_ids.timebox_initial_date_end",
+        "task_ids.timebox_upcoming_id",
+        "task_ids.timebox_upcoming_date_start",
+        "task_ids.timebox_upcoming_date_end",
     )
+    def _compute_timebox(self):
+        for document in self:
+            timebox_latest_id = (
+                timebox_latest_date_start
+            ) = (
+                timebox_latest_date_end
+            ) = (
+                timebox_initial_id
+            ) = timebox_initial_date_start = timebox_initial_date_end = False
+
+            if document.task_ids:
+                tasks = document.task_ids
+                latest_sorted = tasks.sorted(key=lambda r: r.timebox_latest_id)
+                timebox_latest_id = latest_sorted[-1].timebox_latest_id
+                timebox_latest_date_start = timebox_latest_id.date_start
+                timebox_latest_date_end = timebox_latest_id.date_end
+                initial_sorted = tasks.sorted(key=lambda r: r.timebox_initial_id)
+                timebox_initial_id = initial_sorted[0].timebox_initial_id
+                timebox_initial_date_start = timebox_initial_id.date_start
+                timebox_initial_date_end = timebox_initial_id.date_end
+
+            document.timebox_latest_id = timebox_latest_id
+            document.timebox_latest_date_start = timebox_latest_date_start
+            document.timebox_latest_date_end = timebox_latest_date_end
+            document.timebox_initial_id = timebox_initial_id
+            document.timebox_initial_date_start = timebox_initial_date_start
+            document.timebox_initial_date_end = timebox_initial_date_end
