ea08887ff07d53804ec71009ba7aa7e6dacf1f2c
Andhitia Rama <andhitia.r@gmail.com>
2023-05-13 03:53:09 +0700

    [ssi_helpdesk_odoo_implementation] 14.0.1.1.0

--- ssi_helpdesk_odoo_implementation/models/helpdesk_ticket.py
+++ ssi_helpdesk_odoo_implementation/models/helpdesk_ticket.py
@@ -15,9 +15,37 @@ class HelpdeskTicket(models.Model):
         string="# Odoo Implementation",
         comodel_name="odoo_implementation",
     )
+    odoo_version_id = fields.Many2one(
+        string="Odoo Version",
+        comodel_name="odoo_version",
+        related="odoo_implementation_id.version_id",
+        store=True,
+    )
+    odoo_feature_id = fields.Many2one(
+        string="Related Feature",
+        comodel_name="odoo_feature",
+    )
+    odoo_feature_issue_id = fields.Many2one(
+        string="Related Issue",
+        comodel_name="odoo_feature_issue",
+    )
 
     @api.onchange(
         "commercial_partner_id",
     )
     def onchange_odoo_implementation_id(self):
         self.odoo_implementation_id = False
+
+    @api.onchange(
+        "odoo_feature_id",
+    )
+    def onchange_odoo_feature_issue_id(self):
+        self.odoo_feature_issue_id = False
+
+    @api.onchange(
+        "odoo_feature_issue_id",
+    )
+    def onchange_task_ids(self):
+        self.task_ids = False
+        if self.odoo_feature_issue_id:
+            self.task_ids = self.odoo_feature_issue_id.task_ids
