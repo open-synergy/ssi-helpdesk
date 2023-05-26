e9efd744f1753041574d9150552b997bea0b6f21
mikevhe18 <viriyananda.michael@gmail.com>
2023-05-26 17:35:04 +0700

    [ssi_helpdesk_portal] QC

--- ssi_helpdesk_portal/controller/main.py
+++ ssi_helpdesk_portal/controller/main.py
@@ -4,39 +4,42 @@
 
 from odoo import http
 from odoo.http import request
-from odoo.addons.website_form.controllers.main import WebsiteForm
+
 from odoo.addons.website_form.controllers.main import WebsiteForm
 
 
 class HelpdeskPortal(http.Controller):
-
-    @http.route(['/helpdesk/', '/helpdesk/<model("helpdesk_ticket"):ticket>'], type='http', auth="public", website=True,
-                sitemap=True)
+    @http.route(
+        ["/helpdesk/", '/helpdesk/<model("helpdesk_ticket"):ticket>'],
+        type="http",
+        auth="public",
+        website=True,
+        sitemap=True,
+    )
     def ssi_helpdesk_portal(self, ticket=None, **kwargs):
-        search = kwargs.get('search')
+        kwargs.get("search")
         # For breadcrumb index: get all ticket
-        tickets = request.env['helpdesk_ticket'].search([], order="id asc")
-        result = {'ticket': ticket}
+        tickets = request.env["helpdesk_ticket"].search([], order="id asc")
+        result = {"ticket": ticket}
         # For breadcrumb index: get all ticket
-        result['tickets'] = tickets
+        result["tickets"] = tickets
         return request.render("ssi_helpdesk_portal.ticket", result)
 
 
 class WebsiteForm(WebsiteForm):
-
     def _handle_website_form(self, model_name, **kwargs):
         context = request.env.context.copy()
         attachments = []
         for key, value in kwargs.items():
-            if key[:11] == 'attachments':
+            if key[:11] == "attachments":
                 attachments.append(value)
         values = {
-            'from_website': True,
-            'title': kwargs['title'],
-            'partner_id': request.env.user.partner_id.id,
-            'user_id': request.env.ref('base.user_admin').id,
-            'description': kwargs.get('description'),
-            'attachments': attachments,
+            "from_website": True,
+            "title": kwargs["title"],
+            "partner_id": request.env.user.partner_id.id,
+            "user_id": request.env.ref("base.user_admin").id,
+            "description": kwargs.get("description"),
+            "attachments": attachments,
         }
         context.update(values)
         request.env.context = context
